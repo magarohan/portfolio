1912c8fdf21c758ff4df0b05dd8fee86d088f28d
magarohan <magarrohan193@gmail.com>
2024-06-25 10:33:26 +0545

    [feature]: adds socials to the about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,13 +1,15 @@
 import React from "react";
 import me from '../assets/pfp.png'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function About() {
   return (
-    <div className="aboutpage">
+    <div className="about-page">
       <div className="content">
         <img src={me} alt="Rohan Magar" className="profile-pic-about" />
         <div className="info">
-        <div className="about">About me</div>
+          <div className="about">About me</div>
           <div className="info-item">
             <span className="heads">Name: </span>
             <span className="values">Rohan Magar</span>
@@ -23,6 +25,17 @@ function About() {
           <div className="info-item">
             <span className="heads">E-mail: </span>
             <span className="values">magarrohan193@gmail.com</span>
+          </div>
+          <div className="socials">
+            <a href="https://www.instagram.com/_rohan.magar_/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="2x"/>
+            </a>
+            <a href="https://github.com/magarohan" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} size="2x"/>
+            </a>
+            <a href="https://www.linkedin.com/in/rahan-raj-magar/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+            </a>
           </div>
         </div>
       </div>
